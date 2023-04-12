c78d14bced4c0ccd9b4bea72e25284565f39fba2
tlightk <klayxi@gmail.com>
2023-04-12 16:16:07 +0900

    updated onClick function and added state for item index

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
+
 export default function ListGroup() {
-  const items = ["Item 1", "Item 2", "Item 3"];
+  let items = ["Item 1", "Item 2", "Item 3"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log("Clicked")}
+            onClick={() => setSelectedIndex(index)}
           >
             {item}
           </li>
